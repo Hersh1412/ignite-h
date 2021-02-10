c2f23875c9afb4707789790aa06c98f67def4ab9
herschelle <herschelle21@gmail.com>
2021-02-10 18:20:29 +0530

    style cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,57 @@
 // Components
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 // Custom Components
 import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 // Function
 const Home = () => {
+  // fetch data from api to redux store
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  //get data from redux store
+  const { popularGames, newGames, upcomingGames } = useSelector(
+    (state) => state.games
+  );
 
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <StyledGameList>
+      <h1>Upcoming Games</h1>
+      <StyledGames>
+        {upcomingGames.map((item) => (
+          <Game
+            name={item.name}
+            released={item.released}
+            id={item.id}
+            image={item.background_image}
+            key={item.id}
+          />
+        ))}
+      </StyledGames>
+    </StyledGameList>
   );
 };
+
+const StyledGameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const StyledGames = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
